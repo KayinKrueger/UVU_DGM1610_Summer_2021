fbe97141ec4f2268de07ec79e7c8833c6316cbb7
KayinKrueger <84166544+KayinKrueger@users.noreply.github.com>
2021-06-11 23:28:39 -0600

    Create Operators.cs

---
+++ Operators.cs
@@ -0,0 +1,12 @@
+int x = 943;
+Console.WriteLine(x);
+
+int y = 468;
+Console.WriteLine(y);
+
+int sum1 = x + y;
+Console.WriteLine(x + y)
+
+int x = 943;
+int y = 468;
+Console.WriteLine(x > y) // returns ture because x is greater than y
