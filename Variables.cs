aa44ba6aa9299c30e30813400d2c2f6024630db0
KayinKrueger <84166544+KayinKrueger@users.noreply.github.com>
2021-06-11 23:21:12 -0600

    Create Variables.cs

---
+++ Variables.cs
@@ -0,0 +1,18 @@
+int myNum;
+myNum= 123;
+Console.WriteLine(myNum);
+
+float myNum = 123.123D;
+Console.WriteLine(myNum);
+
+string name = "Kayin";
+Console.WriteLine(name);
+
+char myLetter = 'D';
+Console.WriteLine(myLetter);
+
+bool myBool = Ture;
+Console.WriteLine(myBool);
+
+string myText = "Coding is very hard";
+Console.WriteLine(myText); //This coding stuff is not what I had in mind for animation
